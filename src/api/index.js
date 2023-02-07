8e8493e83eb3f6643c410342abbb185cb49f9cfd
Yeabtsega Behailu <91087178+Yeabtsega1@users.noreply.github.com>
2023-02-07 20:13:08 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://stackoverflowback.onrender.com '})
+const API = axios.create({ baseURL: 'https://stackoverflowback.onrender.com'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
